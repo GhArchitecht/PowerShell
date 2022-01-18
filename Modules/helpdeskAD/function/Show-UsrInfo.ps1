f422cfec42156546eadf2cf2448463d8f4fb9c41
Joe Dwumfour <6957676+GhArchitecht@users.noreply.github.com>
2022-01-18 09:20:49 +0000

    Update Show-UsrInfo.ps1
    Update Comment-Based Help

--- Modules/helpdeskAD/function/Show-UsrInfo.ps1
+++ Modules/helpdeskAD/function/Show-UsrInfo.ps1
@@ -1,5 +1,24 @@
 function Show-UsrInfo
 {
+     <#
+    .SYNOPSIS
+        Shows a custom profile for one or more Active Directory users.
+
+    .DESCRIPTION
+        The Show-UsrInfo cmdlet gets a specified user object or performs a search to get multiple user objects.
+
+    .PARAMETER $usr
+        The usr parameter specifies the Active Directory user to get. You can identify a user by its Security Account Manager (SAM) account name.
+
+    .INPUT
+        This function does not support piping.
+
+    .OUTPUT
+        This cmdlet returns custom property values of an Active Directory user.
+
+    .EXAMPLE
+        PS> Show-UsrInfo -$usr JDoe
+    #>
   
     [CmdletBinding()]
         param (
@@ -15,7 +34,7 @@ function Show-UsrInfo
   
         #Check if the user exists or if the username is valid. Do not show the result on the screen.
         try {
-             $null =  Get-ADUser -Identity $usr -ErrorAction Stop
+             $null = Get-ADUser -Identity $usr -ErrorAction Stop
             }
 
         catch {   
@@ -23,47 +42,55 @@ function Show-UsrInfo
             return $null
           }
 
-    #
-    $UserProperties = Get-ADUser -Identity $usr -Properties *
+        #Gets all the properties of a user
+        $UserProperties = Get-ADUser -Identity $usr -Properties *
+        
+        #Gets users the manager's property name
+        $UserManager = $UserProperties | Select -ExpandProperty Manager
 
-    $UserManager = $UserProperties | Select -ExpandProperty Manager
-    $newUserManager = Get-ADUser $UserManager -Properties *
-    $newUserManagerName = $newUserManager.GivenName + " " + $newUserManager.Surname
+        #Gets all the properties of a user's manager 
+        $newUserManager = Get-ADUser $UserManager -Properties *
+
+        #Gets the users manager first and last name
+        $newUserManagerName = $newUserManager.GivenName + " " + $newUserManager.Surname
 
 
-    $UserAccountExpiredDate = $UserProperties.AccountExpirationDate
-    $CurrentDate = Get-Date
-    $UserAccountExpiredDateResults = If ($UserAccountExpiredDate -lt $CurrentDate -and  -not($UserAccountExpiredDate -eq $null))
-                                        {  
-                                             'Account expired on ' + $UserAccountExpiredDate
-                                        }
-                                        else {
-                                             'Account has not expired.' + ' Date account expires is on '+ $UserAccountExpiredDate
-                                        }
+        $UserAccountExpiredDate = $UserProperties.AccountExpirationDate
+        $CurrentDate = Get-Date
 
-    $CalculatePasswordAge = New-TimeSpan -start $UserProperties.PasswordLastSet -end $CurrentDate
-    $PasswordAge = "$($CalculatePasswordAge.Days) $("day(s) old")"
+        #Checks if a users account has expired or not expired
+        $UserAccountExpiredDateResults = If ($UserAccountExpiredDate -lt $CurrentDate -and  -not($UserAccountExpiredDate -eq $null))
+                                            {  
+                                                'Account expired!!! ' + '' + $UserAccountExpiredDate
+                                            }
+                                            else {
+                                                'Account is active and expires on ' +  $UserAccountExpiredDate
+                                            }
 
-    $ProfileInfo = [PSCustomObject]@{
-          'Full Name:' = $UserProperties.GivenName + ", " + $UserProperties.Surname
-          'Username:' = $UserProperties.SamAccountName
-          'Email Address:' = $UserProperties.EmailAddress
-          'Department:' = $UserProperties.Department
-          'Country' = $UserProperties.Country
-          'Country 2' = $UserProperties.co
-          'Employee Role' = $UserProperties.EmployeeType
-          'Title' = $UserProperties.Title
-          'Manager' = $newUserManagerName
-          'Managers Username' = $newUserManager.SamAccountName
-          'Account Created on ' = $UserProperties.Created
-          'Account Enabled' = $UserProperties.Enabled | BoolToYesNo
-          'Account Locked out' = $UserProperties.lockedout | BoolToYesNo
-          'Account Expiration Date' = $UserAccountExpiredDateResults 
-          'Password Last Set' = $UserProperties.PasswordLastSet
-          'Password Age:' = $PasswordAge 
-          'Password set to  never expire' = $UserProperties.PasswordNeverExpires  | BoolToYesNo
-          'Last Bad Password Set' = $UserProperties.LastBadPasswordattempt 
-    }
+        $CalculatePasswordAge = New-TimeSpan -start $UserProperties.PasswordLastSet -end $CurrentDate
+        $PasswordAge = "$($CalculatePasswordAge.Days) $("day(s) old")"
+
+
+        $ProfileInfo = [PSCustomObject]@{
+            'Full Name:' = $UserProperties.GivenName + ", " + $UserProperties.Surname
+            'Username:' = $UserProperties.SamAccountName
+            'Email Address:' = $UserProperties.EmailAddress
+            'Department:' = $UserProperties.Department
+            'Country' = $UserProperties.Country
+            'Country 2' = $UserProperties.co
+            'Employee Role' = $UserProperties.EmployeeType
+            'Title' = $UserProperties.Title
+            'Manager' = $newUserManagerName
+            'Managers Username' = $newUserManager.SamAccountName
+            'Account Created on ' = $UserProperties.Created
+            'Account Enabled' = $UserProperties.Enabled | BoolToYesNo
+            'Account Locked out' = $UserProperties.lockedout | BoolToYesNo
+            'Account Expiration Date' = $UserAccountExpiredDateResults 
+            'Password Last Set' = $UserProperties.PasswordLastSet
+            'Password Age:' = $PasswordAge 
+            'Password set to  never expire' = $UserProperties.PasswordNeverExpires  | BoolToYesNo
+            'Last Bad Password Set' = $UserProperties.LastBadPasswordattempt 
+         }
 
     Write-Output $ProfileInfo | FL
 }
